0fbfbc4ed65957b6fe132242dae968948ecf927e
unknown <wabrodor@gmail.com>
2022-01-31 10:16:25 +0200

    message

--- pages/about.js
+++ pages/about.js
@@ -1,30 +1,45 @@
 import React from "react";
-import UseSwr from "swr";
+import useSWR from "swr";
 import Link from 'next/link';
 import styles from "./Login.module.css"
+import Image from "next/image";
+import sunshine from "./sun.jpg"
+export default function about() { 
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
-
-export default function about() {
-    const { data, error } = UseSwr("/api/user", fetcher);
-
-    if (error) return <div>Failed to load users</div>;
-    if (!data) return <div>Loading...</div>
 
   return (
     <div>
       <Link href="/">
         <a className={styles.hello}> home </a>
       </Link>
+      <Image
+        src={sunshine}
+        alt="Picture of the author"
+        width={500}
+        height={500}
+      />
    
       <p className={styles.hello}>hi my brother</p>
-      {data.map( item =>{
-          return(
-              <p key={item.id}>{item.user}</p>
-          )
-      })}
+      {/* {data.map((item) => {
+        return <p key={item.id}>{item.user}</p>;
+      })} */}
     </div>
   );
  
 }
+
+
+// export const getServerSideProps = async () => {
+//   const fetcher = (url) => fetch(url).then((res) => res.json());
+//   const { data, error } = useSWR("/api/user", fetcher);
+
+//   if (error) return <div>Failed to load users</div>;
+//   if (!data) return <div>Loading...</div>;
+
+//   return {
+//     props: {
+//       user: user,
+//     },
+//   };
+// };
 
--- pages/api/user.js
+++ pages/api/user.js
@@ -16,7 +16,7 @@ export default function handler(req, res) {
   if (req.method === "GET") {
       res.status(200).json(user);
   } else {
-    console.log(EvalError);
+    console.log("not good");
   }
 
 }
